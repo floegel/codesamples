6cdee7b54cd0115d5934e056d871d4698427a8aa
floegel <s42293@beuth-hochschule.de>
2014-08-27 21:28:06 +0200

    Small fix

--- 1_Invoicing.php
+++ 1_Invoicing.php
@@ -10,59 +10,59 @@
  * [2] vat 19%, sum 100eur
  * and a discount of 10%, the returned array will look like this:
  * array(4) {
- * 	 ["subtotal"]=> float(200)
- * 	 ["total"]=> float(203,4)
- * 	 ["discount"]=> array(3) { ["perc"]=> int(10) ["of"]=> float(200) ["total"]=> float(20) }
- * 	 ["vats"]=> array(2) {
- * 	   [7]=> array(2) { ["of"]=> float(90) ["total"]=> float(6,3) }
- * 	   [19]=> array(2) { ["of"]=> float(90) ["total"]=> float(17,1) }
- * 	 }
+ *     ["subtotal"]=> float(200)
+ *     ["total"]=> float(203,4)
+ *     ["discount"]=> array(3) { ["perc"]=> int(10) ["of"]=> float(200) ["total"]=> float(20) }
+ *     ["vats"]=> array(2) {
+ *        [7]=> array(2) { ["of"]=> float(90) ["total"]=> float(6,3) }
+ *        [19]=> array(2) { ["of"]=> float(90) ["total"]=> float(17,1) }
+ *     }
  * }
  */
 public function calculateInvoiceValues(InvoiceBundle_Models_Invoice $invoice)
 {
-	$invoiceItems = $invoice->getItems();
-	$discountPerc = intval($invoice->getDiscount()); // discount in %
+    $invoiceItems = $invoice->getItems();
+    $discountPerc = intval($invoice->getDiscount()); // discount in %
 
-	$subtotal = 0;
-	$total = 0;
-	$vatTotal = 0;
-	$vatList = array();
-	$discountTotal = 0;
+    $subtotal = 0;
+    $total = 0;
+    $vatTotal = 0;
+    $vatList = array();
+    $discountTotal = 0;
 
-	foreach ($invoiceItems as $item)
-	{
-		$finalPrice = ($item->getUnits() * $item->getPrice());
-		$vatPerc = intval($item->getVat());
+    foreach ($invoiceItems as $item)
+    {
+        $finalPrice = ($item->getUnits() * $item->getPrice());
+        $vatPerc = intval($item->getVat());
 
-		$subtotal += $finalPrice;
+        $subtotal += $finalPrice;
 
-		if ($discountPerc > 0)
-		{
-			$partialDiscount = ($finalPrice * $discountPerc / 100);
-			$discountTotal += $partialDiscount;
-			$finalPrice -= $partialDiscount;
-		}
+        if ($discountPerc > 0)
+        {
+            $partialDiscount = ($finalPrice * $discountPerc / 100);
+            $discountTotal += $partialDiscount;
+            $finalPrice -= $partialDiscount;
+        }
 
-		$partialVat = ($finalPrice * $vatPerc / 100);
-		$vatTotal += $partialVat;
+        $partialVat = ($finalPrice * $vatPerc / 100);
+        $vatTotal += $partialVat;
 
-		if ($vatPerc > 0)
-		{
-			if (!isset($vatList[$vatPerc]))
-			{
-				$vatList[$vatPerc] = array("of" => 0, "total" => 0);
-			}
-			$vatList[$vatPerc]["of"] += $finalPrice;
-			$vatList[$vatPerc]["total"] += $partialVat;
-		}
-	}
+        if ($vatPerc > 0)
+        {
+            if (!isset($vatList[$vatPerc]))
+            {
+                $vatList[$vatPerc] = array("of" => 0, "total" => 0);
+            }
+            $vatList[$vatPerc]["of"] += $finalPrice;
+            $vatList[$vatPerc]["total"] += $partialVat;
+        }
+    }
 
-	$result = array(
-		"subtotal" => $subtotal,
-		"total" => ($subtotal - $discountTotal + $vatTotal),
-		"discount" => array("perc" => $discountPerc, "of" => $subtotal, "total" => $discountTotal),
-		"vats" => $vatList
-	);
-	return $result;
+    $result = array(
+        "subtotal" => $subtotal,
+        "total" => ($subtotal - $discountTotal + $vatTotal),
+        "discount" => array("perc" => $discountPerc, "of" => $subtotal, "total" => $discountTotal),
+        "vats" => $vatList
+    );
+    return $result;
 }
